17e13a0497ca29c4ca8eea3fd874281d29f3fb0e
JohnzzzAntony <johnsjoantony@gmail.com>
2026-05-25 11:36:15 +0400

    migrations: synchronize model changes and index optimizations

--- core/migrations/0036_designsettings_enable_custom_cursor_and_more.py
+++ core/migrations/0036_designsettings_enable_custom_cursor_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0035_designsettings_hp_categories_badge_and_more'),
+        ('core', '0037_flower_district_theme_redesign'),
     ]
 
     operations = [
